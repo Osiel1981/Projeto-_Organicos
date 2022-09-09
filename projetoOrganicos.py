2cf5f09ab9b9cffe75a4e3db520bf901135b08c0
anderson <andinho@gmail.com>
2022-09-09 15:01:44 -0300

    Inclui tela de Boas vindas de teste

--- projetoOrganicos.py
+++ projetoOrganicos.py
@@ -1,21 +1,18 @@
 
+from re import A
 carrinho = 0
 
 somaCarrinho = 0
 
-from re import A
 produtos = dict()
-Estoque = ['maça':2.5, 'pera':3]
+Estoque = {'maça': 2.5, 'pera': 3}
 preco = 0
 
 
-print('''
-==================================
-Bem Vindo ao Desefio Organicos!!!
-==================================''')
+
 
 opcao = "a"
-while opcao.upper() != "S": 
+while opcao.upper() != "S":
     print('''
     -----------------
     Menu de navegação
@@ -26,7 +23,7 @@ while opcao.upper() != "S":
     S = sair e fechar o programa
     -----------------''')
     opcao = input("Digite o que deseja acessar: ").upper()
-    
+
     if opcao == 'C':
         print('''
     -----------------
@@ -37,31 +34,30 @@ while opcao.upper() != "S":
     V = Voltar para o menu anterior
     S = Sair do programa
     -----------------''')
-    
+
     opcao = input("Digite o que deseja acessar: ")
-    if not opcao.upper() in "CVRS": 
-        
+    if not opcao.upper() in "CVRS":
+
         print('''
         _______________
         OPÇÃO INVALIDA
         _______________''')
-    
+
     opcao = input("Digite o que deseja acessar: ").upper()
     if opcao == 'C':
-    
-    
+
         novo_produto = input("Qual o nome do novo produto")
         while len(novo_produto) <= 5:
             print("nome não pode conter menos de 5 caracteres'.")
             input("Favor inserir um nome válido")
         produtos['produto'] = novo_produto
-        
-         preco_produto = float(input("Qual o preço atualizado? \nR$:"))
+
+        preco_produto = float(input("Qual o preço atualizado? \nR$:"))
         while preco_produto <= 0:
             print("Valor deve ser maior que R$ 0")
             input("Favor inserir valor válido: R$")
         produtos['preco'] = preco_produto
-        
+
         Fabricante = input("Qual o fabricante do produto: \n")
         while len(Fabricante) <= 2:
             print("Valor inválido")
@@ -72,17 +68,12 @@ while opcao.upper() != "S":
         while len(fornecedor) <= 3:
             print("Valor inválido")
             input("Favor inserir nome válido:\n")
-        produtos['fornecedor'] = fornecedor    
+        produtos['fornecedor'] = fornecedor
         Estoque.append(produtos.copy())
         print("Produto cadastrado, :", novo_produto)
         print(Estoque)
     else:
         print("opção ainda não cadastrada")
-
-
-
-       
-
 
     if opcao.upper() == "V":
         opcaoVendas = "V"
@@ -103,9 +94,10 @@ while opcao.upper() != "S":
                 maisCarrinho = "S"
 
                 while maisCarrinho.upper() == "S":
-                    produto = input("Digite o nome do produto que deseja adicionar ao carrinho: ")
+                    produto = input(
+                        "Digite o nome do produto que deseja adicionar ao carrinho: ")
                     if produto in produtos.keys():
-                    
+
                         carrinho = carrinho + produtos[produto]
 
                     else:
@@ -115,19 +107,19 @@ while opcao.upper() != "S":
                         ------------------------
                         ''')
 
-
-                    maisCarrinho = input("Deseja adicionar mais produtos ao carrinho? Digite S para sim ou N para não: ")
+                    maisCarrinho = input(
+                        "Deseja adicionar mais produtos ao carrinho? Digite S para sim ou N para não: ")
 
             if opcaoVendas.upper() == "R":
                 menosCarrinho = "S"
                 while menosCarrinho == "S":
-                    produto = input("Digite o nome do produto que deseja remover: ")
-                    
+                    produto = input(
+                        "Digite o nome do produto que deseja remover: ")
+
                     carrinho = carrinho - produtos(produto)
 
-                    menosCarrinho = input("Deseja remover mais produtos do carrinho? Digite S para sim ou N para não ")
-
-
+                    menosCarrinho = input(
+                        "Deseja remover mais produtos do carrinho? Digite S para sim ou N para não ")
 
 
 print('''
@@ -136,4 +128,3 @@ Fim de programa!
 _______________
 ''')
 print(carrinho)
-
