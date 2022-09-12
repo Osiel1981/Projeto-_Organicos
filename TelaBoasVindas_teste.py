579f01eacc98099a890219cd1628d3d8590244ab
anderson <andinho@gmail.com>
2022-09-12 16:05:33 -0300

    tela 3.0

--- TelaBoasVindas_teste.py
+++ TelaBoasVindas_teste.py
@@ -1,39 +1,20 @@
+print('=' * 30),print('-' * 30),print(' ' * 30),print('\\' * 30), print('BEM VINDO AO DESAFIO PYTHON #1' )
+print('//' * 15)
+print(' ' * 15),
+print('-' * 30),print('=' * 30),print ('Orientação :   Matheus Willian'),print('=' * 30)
 print('''
-                  =====================================
-                  !!! Bem Vindo ao Desefio Organicos!!!
-                  =====================================
-                  =====================================
-                  =                                   =
-                  =             .-------.             =
-                  =            _|_O_    |_            =
-                  =           |         | |           =
-                  =           | .-------' |           =
-                  =           |_|    _____|           =
-                  =             |     O |             =
-                  =             '-------'             =
-                  =                                   =
-                  =====================================
-                  =        Primeiro Projeto           =
-                  =               do                  =
-                  =     Div Magalu : Dev - Python     =
-                  =====================================
-                  =  Desnvolvedores:                  =
-                  =                                   =
-                  =                Anderson Teixeira  =
-                  =                Eron Morais        =
-                  =                João Paulo Garcia  =
-                  =                Osiel Mesquita     =
-                  ===================================== 
-                  =                                   =
-                  = Orientação :                      =
-                  =                                   =
-                  =                  Matheus Willian  =
-                  =                                   =
-                  =====================================
-                  =                                   =
-                  ===================================== 
-
-
-
- 
-''')
+          .-------.             
+         _|_O_    |_            
+        |         | |           
+        | .-------' |           
+        |_|    _____|           
+          |     O |           
+          '-------' ''')
+print(''' ''')
+print('=' * 30),print(' ' * 30),print ('''Desnvolvedores:             
+                                                     
+            Anderson Teixeira  
+            Eron Morais        
+            João Paulo Garcia  
+            Osiel Mesquita     '''),print(' ' * 30),print('=' * 30)
+print(' ' * 30)
