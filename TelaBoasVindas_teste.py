b763d0dfe9d829141c406949f89b0ce4990490c6
anderson <andinho@gmail.com>
2022-09-12 11:08:08 -0300

    Tela de boas vindas atualizada

--- TelaBoasVindas_teste.py
+++ TelaBoasVindas_teste.py
@@ -1,39 +1,60 @@
-print('''
-                  =====================================
-                  !!! Bem Vindo ao Desefio Organicos!!!
-                  =====================================
-                  =====================================
-                  =                                   =
-                  =             .-------.             =
-                  =            _|_O_    |_            =
-                  =           |         | |           =
-                  =           | .-------' |           =
-                  =           |_|    _____|           =
-                  =             |     O |             =
-                  =             '-------'             =
-                  =                                   =
-                  =====================================
-                  =        Primeiro Projeto           =
-                  =               do                  =
-                  =     Div Magalu : Dev - Python     =
-                  =====================================
-                  =  Desnvolvedores:                  =
-                  =                                   =
-                  =                Anderson Teixeira  =
-                  =                Eron Morais        =
-                  =                João Paulo Garcia  =
-                  =                Osiel Mesquita     =
-                  ===================================== 
-                  =                                   =
-                  = Orientação :                      =
-                  =                                   =
-                  =                  Matheus Willian  =
-                  =                                   =
-                  =====================================
-                  =                                   =
-                  ===================================== 
+#print('''
+#                  =====================================
+ #                 !!! Bem Vindo ao Desefio Organicos!!!
+  #                =====================================
+   #               =====================================
+    #              =                                   =
+     #             =             .-------.             =
+      #            =            _|_O_    |_            =
+       #           =           |         | |           =
+        #          =           | .-------' |           =
+         #         =           |_|    _____|           =
+          #        =             |     O |             =
+           #       =             '-------'             =
+            #      =                                   =
+             #     =====================================
+              #    =        Primeiro Projeto           =
+               #   =               do                  =
+                #  =     Div Magalu : Dev - Python     =
+                 # =====================================
+#                  =  Desnvolvedores:                  =
+#                  =                                   =
+#                  =                Anderson Teixeira  =
+#                  =                Eron Morais        =
+#                  =                João Paulo Garcia  =
+#                  =                Osiel Mesquita     =
+#                  ===================================== 
+#                  =                                   =
+#                  = Orientação :                      =
+#                  =                                   =
+#                  =                  Matheus Willian  =
+#                  =                                   =
+#                  =====================================
+#                  =                                   =
+#                  ===================================== 
 
 
 
  
-''')
+
+
+print('=' * 30),print('-' * 30),print(' ' * 30),print('\\' * 30), print('BEM VINDO AO DESAFIO PYTHON #1' )
+print('//' * 15)
+print(' ' * 15),
+print('-' * 30),print('=' * 30),print ('Orientação :   Matheus Willian'),print('=' * 30)
+print('''
+          .-------.             
+         _|_O_    |_            
+        |         | |           
+        | .-------' |           
+        |_|    _____|           
+          |     O |           
+          '-------' ''')
+print(''' ''')
+print('=' * 30),print(' ' * 30),print ('''Desnvolvedores:             
+                                                     
+            Anderson Teixeira  
+            Eron Morais        
+            João Paulo Garcia  
+            Osiel Mesquita     '''),print(' ' * 30),print('=' * 30)
+print(' ' * 30)
