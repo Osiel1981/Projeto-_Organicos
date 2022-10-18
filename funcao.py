eb1d17bd8549aa80961eaea91e18917ccb473efd
Osiel <osiel.fotografia@gmail.com>
2022-10-18 14:28:58 -0300

    Adicionei opção lista. para revisão

--- funcao.py
+++ funcao.py
@@ -1,9 +1,15 @@
-from cgitb import html
+'''from cgitb import html
+from flask import render_template
 import pandas as pd
+from flask import Flask
 
-df = pd.read_csv('estoque.csv')
-df_html = df.to_html()
-lista = open("static/lista.html", "w")
-lista.write(df_html)
-lista.close()
+app = Flask(__name__)
+@app.route('/')
+def listar():
+    df = pd.read_csv('estoque.csv')
+    df_html = df.to_html()
+    print(df_html)
+    return render_template('lista.html', table = df_html)
+app.run()
 
+'''
