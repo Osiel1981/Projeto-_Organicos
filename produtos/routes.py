85338d0b3970a5a489cbce5818ad36de77b75ce6
Ettore Mitsueda <ettore.mitsueda@luizalabs.com>
2022-12-12 15:27:52 -0300

    Padronizar prefixo das rotas de produtos

--- produtos/routes.py
+++ produtos/routes.py
@@ -5,10 +5,20 @@ from database import mongo
 from models import Produto, ProdutoUpdate
 
 
-router = APIRouter(tags=["Produtos"])
+router = APIRouter(prefix="/produtos", tags=["Produtos"])
 
 
-@router.post("/cadastrar/", status_code=201, response_model=Produto)
+@router.get("/", status_code=200, response_model=list[Produto])
+def listar_produtos():
+    return list(mongo.coll.find())
+
+
+@router.get("/{doc_id}", status_code=200, response_model=Produto)
+def listar_um_produto(doc_id: str):
+    return mongo.coll.find_one({"_id": doc_id})
+
+
+@router.post("/", status_code=201, response_model=Produto)
 def cadastrar_produto(produto: Produto = Body(example={"nome": "Banana",
                                                        "preco": "4.18",
                                                        "descricao": "Uma dúzia de bananas."})):
@@ -23,17 +33,7 @@ def cadastrar_produto(produto: Produto = Body(example={"nome": "Banana",
     return inserted_doc
 
 
-@router.get("/produtos/", status_code=200, response_model=list[Produto])
-def listar_produtos():
-    return list(mongo.coll.find())
-
-
-@router.get("/produtos/{doc_id}", status_code=200, response_model=Produto)
-def listar_um_produto(doc_id: str):
-    return mongo.coll.find_one({"_id": doc_id})
-
-
-@router.patch("/atualizar/{doc_id}", status_code=200)
+@router.patch("/{doc_id}", status_code=200)
 def atualizar_produto(doc_id: str, produto: ProdutoUpdate):
     produto = json(produto, exclude={"_id": True, "id": True}, exclude_unset=True)
 
@@ -44,7 +44,7 @@ def atualizar_produto(doc_id: str, produto: ProdutoUpdate):
     return HTTPException(404, f"Produto com o ID {doc_id} não existe")
 
 
-@router.delete("/deletar/{doc_id}", status_code=200)
+@router.delete("/{doc_id}", status_code=200)
 def deletar_produto(doc_id: str):
     deleted_doc = mongo.coll.find_one_and_delete({"_id": doc_id})
 
